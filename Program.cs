970889747d0e5e02c901efa5568313f8be5b78c1
Maxime Pages - Aiki <maximepag02@gmail.com>
2024-12-04 10:05:23 +0100

    Day1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AOC.Solvers;
+using System;
+
+namespace AOC;
+
+class Runner
+{
+    const string BasePath = "../../.././Inputs/Day";
+    static void Main()
+    {
+        Day1 day1 = new();
+        Console.WriteLine(day1.Solve(GetInput(1)));
+    }
+
+    static string GetInput(int day)
+    {
+        return System.IO.File.ReadAllText($"{BasePath}{day}");
+    }
+}
---
+++ Solvers/Day1.cs
@@ -0,0 +1,14 @@
+﻿namespace AOC.Solvers;
+
+public class Day1 : Day
+{
+    public override string Solve(string input)
+    {
+        (IEnumerable<int> numbers1, IEnumerable<int> numbers2) = Day.ParseInput2IntLists(input);
+        numbers1 = numbers1.Order();
+        numbers2 = numbers2.Order();
+        return numbers1.Zip(numbers2, (a, b) => Math.Abs(a-b)).Sum().ToString();
+    }
+    
+    
+}
